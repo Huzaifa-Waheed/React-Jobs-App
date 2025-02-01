19c7747f215d885b90efb734aa00cb3719f691ed
Muhammad Huzaifa <huzaifawaheed2727@gmail.com>
2025-02-01 18:44:28 +0500

    Page Not Found added

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import React from "react";
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/Jobs' element={<JobsPage />} />
+        <Route path='*' element={<NotFoundPage />} /> {/* 404 page (* path work for that page which is not exist yet) */}
       </Route>
   )
 )
